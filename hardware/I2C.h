42ef42d2e893b18d8aba990e15d36416524de20c
reme <1242235737@qq.com>
2023-03-29 15:16:54 +0800

    I2C.h-add define: I2C_MODE_DEFAULT,I2C_FAST_DUTY_0,I2C_FAST_DUTY_1,I2C_ACK_Y,I2C_ACK_N

--- hardware/I2C.h
+++ hardware/I2C.h
@@ -3,6 +3,15 @@
 #ifndef _I2C_H
 #define _I2C_H
 
+/*I2C_MODE*/
+#define I2C_MODE_DEFAULT ((uint16_t)0x0000)
+/*FAST_DUTY*/
+#define I2C_FAST_DUTY_0 ((uint16_t)0x8000)
+#define I2C_FAST_DUTY_1 ((uint16_t)0xc000)
+/*I2C_ACK*/
+#define I2C_ACK_Y ((uint16_t)0x0400)
+#define I2C_ACK_N ((uint16_t)0x0000)
+
 typedef struct I2C_init_st
 {
   uint32_t I2C_CLK_SPEED;
