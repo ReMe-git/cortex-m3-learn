6f4c1f06eeecb426106613876f176b923be20146
reme <1242235737@qq.com>
2023-03-29 20:00:24 +0800

    I2C.c-add function: I2C_ack

--- hardware/I2C.c
+++ hardware/I2C.c
@@ -10,6 +10,9 @@
 #define I2C_CR1_STOP_SET ((uint16_t)0x0200)
 #define I2C_CR1_STOP_RESET ((uint16_t)0xfdff)
 
+#define I2C_CR1_ACK_SET ((uint16_t)0x0400)
+#define I2C_CR1_ACK_RESET ((uint16_t)0xfbff)
+
 #define I2C_CR2_FREQ_RESET ((uint16_t)0xffc0)
 
 #define I2C_CCR_FS_RESET ((uint16_t)0x0fff)
@@ -135,4 +138,21 @@ void I2C_stop(I2C_TypeDef *I2Cx,uint8_t state)
 		I2Cx->CR1&= I2C_CR1_STOP_RESET;
 	}
 }
+
+void I2C_ack(I2C_TypeDef *I2Cx,uint8_t state);
+{
+	if(state!= disable)
+	{
+		I2Cx->CR1|= I2C_CR1_ACK_SET;
+	}
+	else
+	{
+		I2Cx->CR1&= I2C_CR1_ACK_RESET;
+	}
+}
+
+void I2C_call(I2C_TypeDef *I2Cx,uint8_t state);
+void I2C_sendData(I2C_TypeDef *I2Cx,uint8_t data);
+void I2C_sendAddress(I2C_TypeDef *I2Cx,uint8_t address);
+uint8_t I2C_receiveData(I2C_TypeDef *I2Cx);
 
