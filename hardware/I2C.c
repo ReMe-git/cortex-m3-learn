13d03437cd58e8ce8d7dc3b7bc9a56029449794c
reme <1242235737@qq.com>
2023-03-29 19:54:57 +0800

    I2C.c-add function: I2C_start

--- hardware/I2C.c
+++ hardware/I2C.c
@@ -3,8 +3,14 @@
 
 #define I2C_CR1_PE_SET ((uint16_t)0x0001)
 #define I2C_CR1_PE_RESET ((uint16_t)0xfffe)
+
+#define I2C_CR1_START_SET ((uint16_t)0x0100) 
+#define I2C_CR1_START_RESET ((uint16_t)0xfeff) 
+
 #define I2C_CR2_FREQ_RESET ((uint16_t)0xffc0)
+
 #define I2C_CCR_FS_RESET ((uint16_t)0x0fff)
+
 #define I2C_CR1_CLEAR_MASK ((uint16_t)0xfbf5)
 
 typedef struct I2C_init_st
@@ -102,4 +108,23 @@ void I2C_cmd(I2C_TypeDef *I2Cx,uint8_t state)
 		I2Cx->CR1&= I2C_PE_RESET;
 	}
 }
+
+void I2C_start(I2C_TypeDef *I2Cx,uint8_t state)
+{
+	if(state!= disable)
+	{
+		I2Cx->CR1|= I2C_CR1_START_SET;
+	}
+	else
+	{
+		I2Cx->CR1&= I2C_CR1_START_RESET;
+	}
+}
+
+void I2C_stop(I2C_TypeDef *I2Cx,uint8_t state);
+void I2C_ack(I2C_TypeDef *I2Cx,uint8_t state);
+void I2C_call(I2C_TypeDef *I2Cx,uint8_t state);
+void I2C_sendData(I2C_TypeDef *I2Cx,uint8_t data);
+void I2C_sendAddress(I2C_TypeDef *I2Cx,uint8_t address);
+uint8_t I2C_receiveData(I2C_TypeDef *I2Cx);
 
