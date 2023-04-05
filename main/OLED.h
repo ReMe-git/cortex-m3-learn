69f9c19d43fb15056c7f6c59e951ac2e51b6b559
reme <1242235737@qq.com>
2023-04-05 11:52:32 +0800

    OLED.h-add struct :add struct line_st store line element in line_st for remove and change

--- main/OLED.h
+++ main/OLED.h
@@ -1,10 +1,20 @@
 #ifndef _OLED_H
 #define _OLED_H
 
+#define MAX_LINE_DOTS_NUMBER 128
+
+typedef struct line_st{
+	uint8_t bg_dot[2];
+	uint8_t ed_dot[2];
+	uint32_t dots_number;
+  uint8_t dots_set[MAX_LINE_DOTS_NUMBER* 2];
+} line_st;
+
+
 void OLED_init(void);
 void OLED_clear(void);
 void OLED_writeCmds(uint8_t *cmd,uint8_t len);
 void OLED_setDots(uint8_t *dots,uint32_t num);
-void OLED_setLine(uint8_t *dot1,uint8_t *dot2);
-void OLED_setLine(uint8_t *dot1,uint8_t *dot2);
+void OLED_rmDots(uint8_t *dots,uint32_t num);
+void OLED_setLine(line_st *line);
 #endif
