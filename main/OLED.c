24a9c139ea4bd41a1a73c7316b80ca4e26080977
reme <1242235737@qq.com>
2023-04-09 21:10:43 +0800

    OLED.c- change function: remove some codes to make the size low

--- main/OLED.c
+++ main/OLED.c
@@ -43,70 +43,67 @@ typedef struct change_st{
 } change_st;
 
 static uint8_t oled_mem[128][8];
+uint8_t setDotType[8];
+uint8_t rmDotType[8];
 
-void OLED_writeCmds(uint8_t *cmd,uint8_t len)
+void OLED_start(void)
 {
-  int i;
-
   I2C_start(I2C1,enable);
 	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT));
 	
   I2C_sendAddress(I2C1,(uint8_t)0x78,I2C_ADDRESS_SEND);
 	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-  
-  for(i= 0;i< len;i++)
-  {
-	  I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,cmd[i]);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	}
-  
+ 
+}
+
+void OLED_stop(void)
+{
   I2C_stop(I2C1,enable);
+	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
+}
+
+void OLED_sendCmd(uint8_t cmd)
+{
+  I2C_sendByte(I2C1,(uint8_t)0x80);
+  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
+	I2C_sendByte(I2C1,cmd);
+	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
+}
+
+void OLED_sendData(uint8_t byte)
+{
+	I2C_sendByte(I2C1,(uint8_t)0xc0);
+	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	
+	I2C_sendByte(I2C1,byte);
+	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
 }
 
 void OLED_clear(void)
 {
   uint32_t i,n;
 
-  I2C_start(I2C1,enable);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT));
+	OLED_start();
 
-	I2C_sendAddress(I2C1,(uint8_t)0x78,I2C_ADDRESS_SEND);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-  
   for(i= 0;i< 8;i++)
   {
- 	  I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,0xb0+ i);	
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    
-    I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,0x00);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));  
-    I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,0x10);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+		OLED_sendCmd((uint8_t)(0xb0+ i));
+		OLED_sendCmd((uint8_t)(0x00));
+		OLED_sendCmd((uint8_t)(0x10));
 
     for(n= 0;n< 128;n++)
     {
-  	  I2C_sendByte(I2C1,(uint8_t)0xc0);
-	    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	  
-      I2C_sendByte(I2C1,(uint8_t)0x00);
-	    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));  
-    }
-  
+			OLED_sendData((uint8_t)0x00);
+
+		}
+
   }
 
-  I2C_stop(I2C1,enable);
+  OLED_stop();
 }
 
 void OLED_memClear(void)
@@ -123,8 +120,32 @@ void OLED_memClear(void)
 	}
 }
 
+void OLED_dataInit(void)
+{
+	OLED_memClear();
+
+	setDotType[0]= 0x01;
+	setDotType[1]= 0x02;
+	setDotType[2]= 0x04;
+	setDotType[3]= 0x08;
+	setDotType[4]= 0x10;
+	setDotType[5]= 0x20;
+	setDotType[6]= 0x40;
+	setDotType[7]= 0x80;
+	rmDotType[0]= 0xfe;
+	rmDotType[1]= 0xfd;
+	rmDotType[2]= 0xfb;
+	rmDotType[3]= 0xf7;
+	rmDotType[4]= 0xef;
+	rmDotType[5]= 0xdf;
+	rmDotType[6]= 0xbf;
+	rmDotType[7]= 0x7f;
+
+}
+
 void OLED_init(void)
 {
+	int i;
   uint8_t init_cmd[28]={0xae,0x20,0x10,0xb0,0x00,0x10,0xc8,0x40,0x81,
                         0xff,0xa1,0xa6,0xa8,0x3f,0xa4,0xd3,0x00,0xd5,
                         0xf0,0xd9,0x22,0xda,0x12,0xdb,0x20,0x8d,0x14,
@@ -149,211 +170,108 @@ void OLED_init(void)
   I2C_cmd(I2C1,enable);
   
   delay_us(100);
-  OLED_writeCmds(init_cmd,27);
-  
+	OLED_start();
+
+	for(i= 0;i< 27;i++)
+	{
+		OLED_sendCmd(init_cmd[i]);
+	}
+	
+	OLED_stop();
+
   OLED_clear();
-	OLED_memClear();
-  OLED_writeCmds(&init_cmd[27],1);
+	OLED_dataInit();
+
+	OLED_start();
+	
+	OLED_sendCmd(init_cmd[i]);
+	
+	OLED_stop();
+
 }
 
 void OLED_close(void)
 {
-   I2C_start(I2C1,enable);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT));
+	OLED_start();
 
-	I2C_sendAddress(I2C1,(uint8_t)0x78,I2C_ADDRESS_SEND);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+	OLED_sendCmd((uint8_t)0xae);
+	
+	OLED_stop();
 
-	I2C_sendByte(I2C1,(uint8_t)0x80);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	  
-	I2C_sendByte(I2C1,(uint8_t)0xae);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-	I2C_stop(I2C1,enable); 
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
 }
 
 void OLED_open(void)
 {
-  I2C_start(I2C1,enable);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT));
+	OLED_start();
+	
+	OLED_sendCmd((uint8_t)0xaf);
 
-	I2C_sendAddress(I2C1,(uint8_t)0x78,I2C_ADDRESS_SEND);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+	OLED_stop();
 
-  I2C_sendByte(I2C1,(uint8_t)0x80);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	  
-	I2C_sendByte(I2C1,(uint8_t)0xaf);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-	I2C_stop(I2C1,enable);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
 }
 
 void OLED_setDot(uint8_t x,uint8_t y)
 {
-  uint8_t dotType[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+	OLED_start();
 
-  I2C_start(I2C1,enable);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT));
+	OLED_sendCmd((uint8_t)(0xb0+ y/ 8));
+	OLED_sendCmd((uint8_t)(0x0f& x));
+	OLED_sendCmd((uint8_t)(0x10| (x>> 4)));
+  oled_mem[x][y/ 8]|= setDotType[y% 8];
+	OLED_sendData(oled_mem[x][y/ 8]);
 
-	I2C_sendAddress(I2C1,(uint8_t)0x78,I2C_ADDRESS_SEND);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-
-  I2C_sendByte(I2C1,(uint8_t)0x80);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-  I2C_sendByte(I2C1,(uint8_t)(0xb0+ y/ 8));	
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    
-  I2C_sendByte(I2C1,(uint8_t)0x80);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-  I2C_sendByte(I2C1,(uint8_t)(0x0f& x));	
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-  I2C_sendByte(I2C1,(uint8_t)0x80);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-  I2C_sendByte(I2C1,(uint8_t)(0x10| (x>> 4)));
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-  I2C_sendByte(I2C1,(uint8_t)0xc0);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-		
-  oled_mem[x][y/ 8]|= dotType[y% 8];
-  I2C_sendByte(I2C1,oled_mem[x][y/ 8]);	
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-	I2C_stop(I2C1,enable);
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	OLED_stop();
 }
 
 void OLED_rmDot(uint8_t x,uint8_t y)
 {
-  uint8_t dotType[8]={0xfe,0xfd,0xfb,0xf7,0xef,0xdf,0xbf,0x7f};
+	OLED_start();
 
-I2C_start(I2C1,enable);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT));
+	OLED_sendCmd((uint8_t)(0xb0+ y/ 8));
+	OLED_sendCmd((uint8_t)(0x0f& x));
+	OLED_sendCmd((uint8_t)(0x10| (x>> 4)));
+  oled_mem[x][y/ 8]|= rmDotType[y% 8];
+	OLED_sendData(oled_mem[x][y/ 8]);
 
-	I2C_sendAddress(I2C1,(uint8_t)0x78,I2C_ADDRESS_SEND);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-
-  I2C_sendByte(I2C1,(uint8_t)0x80);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-  I2C_sendByte(I2C1,(uint8_t)(0xb0+ y/ 8));	
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    
-  I2C_sendByte(I2C1,(uint8_t)0x80);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-  I2C_sendByte(I2C1,(uint8_t)(0x0f& x));	
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-  I2C_sendByte(I2C1,(uint8_t)0x80);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-  I2C_sendByte(I2C1,(uint8_t)(0x10| (x>> 4)));
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-  I2C_sendByte(I2C1,(uint8_t)0xc0);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-		
-  oled_mem[x][y/ 8]&= dotType[y% 8];
-  I2C_sendByte(I2C1,oled_mem[x][y/ 8]);	
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-	I2C_stop(I2C1,enable);
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	OLED_stop();
 }
 
 void OLED_setDots(uint8_t *dots,uint32_t num)
 {
   uint32_t i;
-  uint8_t dotType[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
 
-	I2C_start(I2C1,enable);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT));
-
-	I2C_sendAddress(I2C1,(uint8_t)0x78,I2C_ADDRESS_SEND);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+	OLED_start();
 
 	for(i= 0;i< num;i++)
-  {
-    I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,(uint8_t)(0xb0+ dots[i* 2+ 1]/ 8));	
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    
-    I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,(uint8_t)(0x0f& dots[i* 2+ 0]));	
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	{
+		OLED_sendCmd((uint8_t)(0xb0+ dots[i* 2+ 1]/ 8));
+		OLED_sendCmd((uint8_t)(0x0f& dots[i* 2+ 0]));
+		OLED_sendCmd((uint8_t)(0x10| (dots[i* 2+ 0]>> 4)));
+		oled_mem[dots[i* 2+ 0]][dots[i* 2+ 1]/ 8]|= setDotType[dots[i* 2+ 1]% 8];
+		OLED_sendData(oled_mem[dots[i* 2+ 0]][dots[i* 2+ 1]/ 8]);
+	}
 
-    I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,(uint8_t)(0x10| (dots[i* 2+ 0]>> 4)));
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	OLED_stop();
 
-    I2C_sendByte(I2C1,(uint8_t)0xc0);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-		
-		oled_mem[dots[i* 2+ 0]][dots[i* 2+ 1]/ 8]|= dotType[dots[i* 2+ 1]% 8];
-    I2C_sendByte(I2C1,oled_mem[dots[i* 2+ 0]][dots[i* 2+ 1]/ 8]);	
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-  }
-
-	I2C_stop(I2C1,enable);
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
 }
 
 void OLED_rmDots(uint8_t *dots,uint32_t num)
 {
   uint32_t i;
-  uint8_t dotType[8]={0xfe,0xfd,0xfb,0xf7,0xef,0xdf,0xbf,0x7f};
 
-	I2C_start(I2C1,enable);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_MODE_SELECT));
+	OLED_start();
 
-	I2C_sendAddress(I2C1,(uint8_t)0x78,I2C_ADDRESS_SEND);
-	while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+	for(i= 0;i< num;i++)
+	{
+		OLED_sendCmd((uint8_t)(0xb0+ dots[i* 2+ 1]/ 8));
+		OLED_sendCmd((uint8_t)(0x0f& dots[i* 2+ 0]));
+		OLED_sendCmd((uint8_t)(0x10| (dots[i* 2+ 0]>> 4)));
+		oled_mem[dots[i* 2+ 0]][dots[i* 2+ 1]/ 8]|= rmDotType[dots[i* 2+ 1]% 8];
+		OLED_sendData(oled_mem[dots[i* 2+ 0]][dots[i* 2+ 1]/ 8]);
+	}
 
-  for(i= 0;i< num;i++)
-  {	
-		I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    
-		I2C_sendByte(I2C1,(uint8_t)(0xb0+ dots[i* 2+ 1]/ 8));	
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-    
-    I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,(uint8_t)(0x0f& dots[i* 2+ 0]));	
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+	OLED_stop();
 
-    I2C_sendByte(I2C1,(uint8_t)0x80);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-	
-    I2C_sendByte(I2C1,(uint8_t)(0x10| (dots[i* 2+ 0]>> 4)));
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-    I2C_sendByte(I2C1,(uint8_t)0xc0);
-	  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-		
-		oled_mem[dots[i* 2+ 0]][dots[i* 2+ 1]/ 8]&= dotType[dots[i* 2+ 1]% 8];
-    I2C_sendByte(I2C1,oled_mem[dots[i* 2+ 0]][dots[i* 2+ 1]/ 8]);	
-    while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-  }
-
-	I2C_stop(I2C1,enable);
-  while(!I2C_checkEvent(I2C1,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
 }
 
 void OLED_freshLine(line_st *line)
@@ -405,7 +323,6 @@ void OLED_setLine(line_st *line)
 
 	if(dx>= dy)
 	{
-/*浮点*/
 		float d= 0.0f;
 		float k= (float)dy/ dx;
 
@@ -425,29 +342,9 @@ void OLED_setLine(line_st *line)
 
 		}
 
-/*整形*/
-/*
-		int p= 2* dy- dx;
-		for(count= 0;count< dx;count++)
-		{
-			dots[count* 2+ 0]= (uint8_t)x;
-			dots[count* 2+ 1]= (uint8_t)y;
-			x+= ax;
-
-			p=p+ 2* dy;
-
-			if(p>= 0)
-			{
-				y+= ay;
-				p= p- 2* dx;
-			}
-
-		}
-*/
 	}
 	else
 	{
-/*浮点*/
 		float d= 0.0f;
 		float k= (float)dx/ dy;
 
@@ -466,25 +363,7 @@ void OLED_setLine(line_st *line)
 			}
 
 		}
-/*整形*/
-/*
-		int p= 2* dx- dy;
-		for(count= 0;count< dy;count++)
-		{
-			dots[count* 2+ 0]= (uint8_t)x;
-			dots[count* 2+ 1]= (uint8_t)y;
-			y+= ay;
 
-			p=p+ 2* dx;
-
-			if(p>= 0)
-			{
-				x+= ax;
-				p= p- 2* dy;
-			}
-
-		}
-*/
 	}
 
 	line->dots_number= (uint32_t)(count- 1);
